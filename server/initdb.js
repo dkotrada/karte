6167f9c299f894b85b4fd069bd0c96dfc2c11757
Dieter Konrad <dk@off217.de>
2018-01-11 20:28:05 +0100

    auf es2015 umgestellt

--- server/initdb.js
+++ server/initdb.js
@@ -1,22 +1,23 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 mongoose.Promise = global.Promise;
+
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://127.0.0.1/karte_db';
-const sh = require('shelljs')
+const sh = require('shelljs');
 
 module.exports = {
-    initdb: function (req, res) {
+    initdb: (req, res) => {
         mongoose.connect(url, {useMongoClient: true});
         const connection = mongoose.connection;
         connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
     },
-    populationcheck: function (req, res) {
+    populationcheck: (req, res) => {
         // Connect using MongoClient
-        MongoClient.connect(url, function (err, db) {
+        MongoClient.connect(url, (err, db) => {
             // Use the admin database for the operation
             const adminDb = db.admin();
 
-            db.listCollections({name: "people"}).toArray(function (err, items) {
+            db.listCollections({name: "people"}).toArray((err, items) => {
                 // console.log(items[0].name);
                 if (items[0].name == 'people') {
                     const command = 'mongoimport --db karte_db --collection people --drop --file DATABASE_SEED.json --jsonArray';
@@ -32,4 +33,4 @@ module.exports = {
             return 0;
         });
     }
-}
+};
